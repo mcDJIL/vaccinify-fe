d12f022371f38de83c6d9a4c0de0d27eaf86d2e4
mc DJIL <mdjauharil29@example.com>
2024-05-16 21:17:16 +0700

    update client.jsx

--- src/Utils/client.jsx
+++ src/Utils/client.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { sessionToken } from '../Constants/localStorage';
 
 const client = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/'
+    baseURL: 'https://vaccinify-api-production.up.railway.app/api/'
 });
 
 client.interceptors.request.use((config) => {
